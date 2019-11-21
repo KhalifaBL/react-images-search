73846eb8bf69944b0bb9db5179f1368aa87b7d22
Khalifa Ben Lazrek <khalifalazreg@gmail.com>
2019-11-21 20:39:14 +0100

    Updates

---
+++ precache-manifest.4f87a70136ab2a76d4111dc2a0c466cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a1b10405b61af0edc830a2b1e465c7",
+    "url": "/react-images-search/index.html"
+  },
+  {
+    "revision": "53844aaaa072ad67d8ee",
+    "url": "/react-images-search/static/css/main.3b51f18c.chunk.css"
+  },
+  {
+    "revision": "393f9bcee8770df83b67",
+    "url": "/react-images-search/static/js/2.584ec298.chunk.js"
+  },
+  {
+    "revision": "53844aaaa072ad67d8ee",
+    "url": "/react-images-search/static/js/main.bfcf7564.chunk.js"
+  },
+  {
+    "revision": "65b30e118a3d1042a5bd",
+    "url": "/react-images-search/static/js/runtime-main.65339261.js"
+  }
+]);
